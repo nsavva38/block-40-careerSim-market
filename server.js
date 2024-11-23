45c7b56416a57d86db85489c1566ae5225d5b6cd
Nick Savva <nsavva38@gmail.com>
2024-11-23 14:10:47 -0500

    Added server.js file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(require("morgan")("dev"));
+app.use(express.json());
+
+
+
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `Endpoint not found.`,
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.send(err.message ?? "Sorry, something broke.");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
