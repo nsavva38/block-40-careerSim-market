24b1523a1892531bc3874da0bb7a511a34588b2e
Nick Savva <nsavva38@gmail.com>
2024-11-23 14:40:22 -0500

    New migration and updated index.js to include methods for User

--- prisma/index.js
+++ prisma/index.js
@@ -1,4 +1,29 @@
+const bcrypt = require("bcrypt");
+
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient(); // might have to add the $extend thingy later
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async register(username, password) {
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+          data: {
+            username,
+            password: hashed
+          }
+        })
+        return user;
+      },
+      async login(username, password) {
+        const user = await prisma.user.findUniqueOrThrow({
+          where: { username },
+        })
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid) throw Error("Invalid Password.");
+        return user;
+      },
+    },
+  },
+});
 module.exports = prisma;
 
