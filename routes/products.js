acbac99d9b3f18fc236308ab0cd69dbddb33f5b1
Nick Savva <nsavva38@gmail.com>
2024-11-23 21:28:08 -0500

    Complete 2.0. Included GET requests

--- routes/products.js
+++ routes/products.js
@@ -3,3 +3,31 @@ const router = express.Router();
 module.exports = router;
 
 const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+
+  try{
+    const products = await prisma.product.findMany();
+    res.json(products);
+
+  } catch (e) {
+    next(e);
+  }
+})
+
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const includeOrders = req.user
+    ? { where: { customerId: req.user.id }}
+    : false;
+  try {
+    const product = await prisma.product.findUniqueOrThrow({
+      where: { id: +id },
+      include: { orders: includeOrders },
+    });
+    res.json(product);
+  } catch (e) {
+    next(e);
+  }
+});
